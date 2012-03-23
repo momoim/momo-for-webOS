ca58fe6bae864f8089b2c9999d2d0d143a565c9d
spemoon <lidian.sw@gmail.com>
2012-03-23 19:33:28 +0800

    complete register

---
+++ application/app/assistants/complete-assistant.js
@@ -0,0 +1,96 @@
+var Complete = function() {};
+
+Complete.prototype = {
+    auth : function(name, password, onSubmitSuccess, onSubmitFailure) {
+        var postParams = {
+            realname: name,
+            password: password
+        };
+        new interfaces.Momo().postUserPersonal(postParams, {
+            onSuccess: onSubmitSuccess,
+            onFailure: onSubmitFailure
+        });
+    }
+}
+
+function CompleteAssistant() {
+    this.modelUser = {
+        realname : '',
+        password : ''
+    }
+}
+
+CompleteAssistant.prototype = {
+    setup: function() {
+        var that = this;
+        /*姓名输入框*/
+        this.controller.setupWidget('name', {
+            modelProperty: 'realname',
+            textCase: Mojo.Widget.steModeLowerCase,
+            enterSubmits:  false,
+            autoFocus: true
+        }, this.modelUser);
+        /*密码输入框*/
+        this.controller.setupWidget('password', {
+            modelProperty: 'password',
+            textCase: Mojo.Widget.steModeLowerCase,
+            enterSubmits: false,
+        }, this.modelUser);
+        /*确认密码输入框*/
+        this.controller.setupWidget('password-ensure', {
+            textCase: Mojo.Widget.steModeLowerCase,
+            enterSubmits: false,
+        });
+        /*登录按钮*/
+        this.controller.setupWidget('submit', 
+            this.attributes = {
+                type : Mojo.Widget.activityButton
+            },
+            this.model = {
+                buttonLabel: $L('完成'),
+                buttonClass: 'affirmative',
+                disabled: false
+            }
+        );
+
+        this.buttonSubmit = this.controller.get('submit');
+        this.controller.listen(this.controller.get('submit'), Mojo.Event.tap, this.onSubmitTapped.bind(this));
+    },
+    onSubmitTapped: function(tapEvent) {
+        var _this = this;
+        if(_this.controller.get('name').mojo.getValue() == ''){
+            NotifyHelper.instance().banner('姓名不能为空');
+            _this.buttonSubmit.mojo.deactivate();
+            return;
+        }
+        if(_this.controller.get('password-ensure').mojo.getValue() == ''){
+            NotifyHelper.instance().banner('确认密码不能为空');
+            _this.buttonSubmit.mojo.deactivate();
+            return;
+        }
+        if(_this.controller.get('password').mojo.getValue() != _this.controller.get('password-ensure').mojo.getValue()){
+            NotifyHelper.instance().banner('两次密码输入不一致');
+            _this.buttonSubmit.mojo.deactivate();
+            return;
+        }
+		if (!this.buttonSubmit.spinning) {
+			this.buttonSubmit.mojo.activate();
+			new Complete().auth(this.modelUser.realname, this.modelUser.password, _this.onSubmitSuccess.bind(_this), _this.onSubmitFailure.bind(_this));
+		}
+    },
+    onSubmitSuccess: function(resp) {
+        var _this = this;
+        Global.authInfo.user.status = JSON.parse(resp.request.transport.responseText).user_status;
+        DBHelper.instance().add('authInfo', Global.authInfo);
+        _this.controller.stageController.swapScene('main');
+    },
+    onSubmitFailure: function(resp) {
+        var tipArray = JSON.parse(resp.request.transport.responseText).error.split(':');
+        var tip = tipArray[0];
+        if(tipArray.length === 2){
+            tip = tipArray[1];
+        }
+        NotifyHelper.instance().banner(tip);
+        //Mojo.Log.warn('resp: ' + JSON.stringify(resp));
+    }
+}
---
+++ application/app/assistants/verify-assistant.js
@@ -0,0 +1,180 @@
+var Verify = function() {};
+
+Verify.prototype = {
+    auth : function(mobile, zone_code, verify_code, onVerifySuccess, onVerifyFailure) {
+        var postParams = {
+            mobile: mobile,
+            zone_code: zone_code,
+            verifycode: verify_code
+        };
+        new interfaces.Momo().postRegisterVerify(postParams, {
+            onSuccess: onVerifySuccess,
+            onFailure: onVerifyFailure
+        });
+    },
+    resend : function(mobile, zone_code, onResendSuccess, onResendFailure) {
+        var postParams = {
+            mobile: mobile,
+            zone_code: zone_code
+        };
+        new interfaces.Momo().postResendVerifyCode(postParams, {
+            onSuccess: onResendSuccess,
+            onFailure: onResendFailure
+        });
+    }
+}
+
+function VerifyAssistant() {
+    this.timer = 60;
+    this.timerCount = 0;
+}
+
+VerifyAssistant.prototype = {
+    setup: function() {
+        var that = this;
+        /*区号选择*/
+        var country = CountryHelper.data();
+        var tempCountry = [];
+        for(var i = 0, len = country.length; i < len; i++){
+            var tempObj = {label:$L(country[i][1] + "(+" + country[i][0] + ")" ), value: country[i][0]};
+            tempCountry.push(tempObj);
+        }
+        this.selectorsModel = {currentStatus: Global['signupUser']['zone_code']};
+        this.statuses =  tempCountry;
+        this.controller.setupWidget('country-code',
+           this.attributes = {
+               label: '国家(区号)',
+               labelPlacement: Mojo.Widget.labelPlacementRight,
+               choices: this.statuses,
+               modelProperty:'currentStatus'
+           },
+           this.model = this.selectorsModel);
+        this.controller.listen('country-code', Mojo.Event.propertyChange, this.changed.bindAsEventListener(this));
+        /*手机号输入框*/
+        this.controller.setupWidget('mobile', {
+            modelProperty: 'mobile',
+            textCase: Mojo.Widget.steModeLowerCase,
+            enterSubmits: true 
+        }, Global['signupUser']);
+        /*验证码输入框*/
+        this.controller.setupWidget('verify-code', {
+            modelProperty: 'password',
+            textCase: Mojo.Widget.steModeLowerCase,
+            enterSubmits: true,
+            autoFocus: true
+        }, Global['signupUser']);
+        /*登录按钮*/
+        this.controller.setupWidget('signup-login', 
+            this.attributes = {
+                type : Mojo.Widget.activityButton
+            },
+            this.model = {
+                buttonLabel: $L('登录'),
+                buttonClass: 'affirmative',
+                disabled: false
+            }
+        );
+
+        this.buttonSignLogin = this.controller.get('signup-login');
+        this.controller.listen(this.controller.get('signup-login'), Mojo.Event.tap, this.onSignupLoginTapped.bind(this));
+        /*重发验证码按钮*/
+        this.controller.setupWidget('resend-verifycode', 
+            this.attributes = {
+                type : Mojo.Widget.activityButton
+            },
+            this.model = {
+                buttonLabel: $L('重发验证码'),
+                buttonClass: 'affirmative',
+                disabled: false
+            }
+        );
+
+        this.buttonSignLogin = this.controller.get('resend-verifycode');
+        this.controller.listen(this.controller.get('resend-verifycode'), Mojo.Event.tap, this.onResendVerifyCodeTapped.bind(this));
+
+    },
+    changed: function(propertyChangeEvent) {
+        Global['signupUser']['zone_code'] = this.selectorsModel.currentStatus;
+    },
+    onSignupLoginTapped: function(tapEvent) {
+        var that = this;
+		Mojo.Log.info(this.TAG, 'onLoginTapped');
+		if (!this.buttonSignLogin.spinning) {
+			this.buttonSignLogin.mojo.activate();
+			new Verify().auth(Global['signupUser']['mobile'], Global['signupUser']['zone_code'], Global['signupUser']['password'], that.onVerifySuccess.bind(that), that.onVerifyFailure.bind(that));
+		}
+    },
+    onResendVerifyCodeTapped: function() {
+        var _this = this;
+        clearTimeout(_this.timerCount);
+        Mojo.Log.warn('flag======>' + Global['signupUser']['get_verify_code_flag'])
+        if(!Global['signupUser']['get_verify_code_flag']){
+            _this.controller.get('resend-verifycode').mojo.activate();
+            new Signup().auth(Global['signupUser']['mobile'], Global['signupUser']['zone_code'], _this.onResendSuccess.bind(_this), _this.onResendFailure.bind(_this));
+        }else{
+            NotifyHelper.instance().banner('请在' + _this.timer + '秒后再点击获取验证码');
+        }
+        (function timerFun(limitTime) {
+            if(_this.timer == 0){
+                Global['signupUser']['get_verify_code_flag'] = false;
+                _this.timer = 60;
+                clearTimeout(_this.timerCount);
+                _this.controller.get('resend-verifycode').mojo.deactivate();
+                Mojo.Log.warn('resend');
+                return;
+            }else{
+                Global['signupUser']['get_verify_code_flag'] = true;
+                _this.timer -= 1;
+                _this.timerCount = setTimeout(function(){timerFun(_this.timer);}, 1000);
+            }   
+        })(_this.timer);
+        //NotifyHelper.instance().banner(timerNum);
+    },
+    onVerifySuccess: function(resp) {
+        var userObj = JSON.parse(resp.request.transport.responseText);
+        Global['signupUser']['uid'] = userObj.uid;
+        Global['signupUser']['user_status'] = userObj.user_status;
+        Global['signupUser']['oauth_token'] = userObj.oauth_token;
+        Global['signupUser']['oauth_token_secret'] = userObj.oauth_token_secret;
+        Global['signupUser']['qname'] = userObj.qname;
+
+        Global.authInfo = {
+			user: {
+				id: Global['signupUser']['uid'],
+				name: Global['signupUser']['realname'],
+				avatar: ''
+			},
+			oauthToken: Global['signupUser']['oauth_token'],
+			tokenSecret: Global['signupUser']['oauth_token_secret'],
+			queueName: Global['signupUser']['qname']
+		};
+        Mojo.Log.warn('uid2222 =========> ' + Global['signupUser']['uid']);
+		this.controller.stageController.popScenesTo('login', {
+            mobile : Global['signupUser']['mobile'],
+            password : Global['signupUser']['password'],
+            user_status : Global['signupUser']['user_status'],
+            action: 'after-signup' 
+        });
+        //NotifyHelper.instance().banner("success");
+    },
+    onVerifyFailure: function(resp) {
+        var tipArray = JSON.parse(resp.request.transport.responseText).error.split(':');
+        var tip = tipArray[0];
+        if(tipArray.length === 2){
+            tip = tipArray[1];
+        }
+        NotifyHelper.instance().banner(tip);
+    },
+    onResendSuccess: function() {
+		//this.controller.stageController.pushScene('verify');
+        NotifyHelper.instance().banner("验证码已发送，请稍等");
+    },
+    onResendFailure: function(resp) {
+        var tipArray = JSON.parse(resp.request.transport.responseText).error.split(':');
+        var tip = tipArray[0];
+        if(tipArray.length === 2){
+            tip = tipArray[1];
+        }
+        NotifyHelper.instance().banner(tip);
+    }
+}
