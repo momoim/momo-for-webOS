11a6c167c11b0b4d639ccc6d2de4adff031f1357
spemoon <lidian.sw@gmail.com>
2012-04-11 20:51:26 +0800

    add recording style

--- application/app/assistants/conv-detail-assistant.js
+++ application/app/assistants/conv-detail-assistant.js
@@ -538,7 +538,7 @@ var ConvDetailAssistant = Class.create({
 	},
 	onRecordStart: function() {
 		var self = this;
-
+        self.controller.get('recording').style.display = 'block';
 		//start recording
 		self.audioFile = 'temply_' + guidGenerator();
 		this.captureHelper.startRecording(self.audioFile, function(response) {
@@ -576,6 +576,7 @@ var ConvDetailAssistant = Class.create({
 	},
 	onRecordEnd: function() {
 		var self = this;
+        self.controller.get('recording').style.display = 'none';
 
 		var wavfile = VR_FOLDER + self.audioFile + VR_EXTENSION;
 		var amrfile = VR_FOLDER + self.audioFile + VR_EXTENSION_AMR;
